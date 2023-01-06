3b99d272a644abb71263db0def2134dfdc138918
H <bluebat@gmail.com>
2023-01-06 09:14:26 +0800

    修复地址错误，

--- src/HTTPRequest.php
+++ src/HTTPRequest.php
@@ -15,7 +15,7 @@ class HTTPRequest
     public function postWithAuth($url,$param)
     {
 
-        $request = $this->http->request("POST",$this->config['token_route'],[
+        $request = $this->http->request("POST",$url,[
             'http_errors' => false, //必须，否则碰到500 400就会返回致命错误，然后放第一个
             'form_params' => $param,
             'auth' => [$this->config['client_id'], $this->config['client_secret'],
