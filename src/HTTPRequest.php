f041d776551b5e74fa18f9ddd8f28fdaf7c48403
H <bluebat@gmail.com>
2023-01-07 16:08:23 +0800

    增加一个接口获取的参数

--- src/HTTPRequest.php
+++ src/HTTPRequest.php
@@ -23,4 +23,17 @@ class HTTPRequest
         ]);
         return $request->getBody();
     }
+
+    public function postApi($url,$param)
+    {
+
+        $request = $this->http->request("POST",$url,[
+            'http_errors' => false, //必须，否则碰到500 400就会返回致命错误，然后放第一个
+            'form_params' => $param,
+            'auth' => [$this->config['client_id'], $this->config['client_secret'],
+            ]
+        ]);
+        return $request->getBody();
+    }
+
 }
